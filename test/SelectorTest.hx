8cfb80d937c6e81b2dac2b337efa1247dec18e14
yannick <yannick.dominguez@gmail.com>
2014-05-31 13:58:55 +0200

    initial commit

---
+++ test/SelectorTest.hx
@@ -0,0 +1,20 @@
+package;
+
+import cocktail.selector.SelectorManager;
+import cocktail.selector.CSSSelectorData;
+import cocktail.selector.SelectorSerializer;
+import cocktail.selector.CSSSelectorParser;
+
+class SelectorTest
+{
+    public function new()
+    {
+
+    }
+
+    @Test
+    public function testSelector()
+    {
+
+    }
+}
